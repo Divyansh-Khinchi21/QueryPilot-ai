43d8e15a80af7d6b898d71939ee4c1f381a9d377
Disha Toshniwal <dishatoshniwal4@gmail.com>
2026-04-23 14:40:35 +0530

    final update

--- db.py
+++ db.py
@@ -1,9 +1,11 @@
+import os
 import mysql.connector
 
 def get_connection():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="@@S12345s@@",
-        database="sales_db"
+        host=os.getenv("MYSQLHOST"),
+        user=os.getenv("MYSQLUSER"),
+        password=os.getenv("MYSQLPASSWORD"),
+        database=os.getenv("MYSQLDATABASE"),
+        port=int(os.getenv("MYSQLPORT"))
     )
